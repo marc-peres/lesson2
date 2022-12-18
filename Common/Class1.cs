8552dc259874656e0976c16eece6471f6c1697f5
Marcos Peres <peres_m@mail.ru>
2022-12-19 00:59:18 +0300

    lesson7

---
+++ Common/Class1.cs
@@ -0,0 +1,83 @@
+﻿namespace Common;
+public static class Helper
+{
+    public static int[,] GetRandom2dArr(int rows, int columns)
+    {
+        int[,] result = new int[rows, columns];
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                result[i, j] = GetRandom(-10, 10);
+            }
+        }
+        return result;
+    }
+    public static int GetRandom(int min, int max)
+    {
+        Random random = new Random();
+        return random.Next(min, max + 1);
+    }
+    public static void PrintArr(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+        Console.WriteLine();
+    }
+
+    public static void PrintArr(string[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+        Console.WriteLine();
+    }
+
+    public static void PrintArr(int[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    public static int PrintElementBy2dArr(int[,] arr, int rowPosition, int columnPosition)
+    {
+        int rows = arr.GetLength(0);
+        int col = arr.GetLength(1);
+        int result = -1;
+        if (rowPosition >= 0 & rowPosition < rows & columnPosition >= 0 & columnPosition < col)
+        {
+            result = arr[rowPosition, columnPosition];
+        }
+        return result;
+    }
+
+    public static void PrintAverageBy2dArrCol(int[,] arr)
+    {
+        int sumByCol = 0;
+        double average = 0;
+        if (arr.GetLength(0) == 0 | arr.GetLength(1) == 0)
+        {
+            Console.WriteLine("массив пуст");
+            return;
+        }
+        for (int i = 0; i < arr.GetLength(1); i++)
+        {
+            for (int j = 0; j < arr.GetLength(0); j++)
+            {
+                sumByCol += arr[j, i];
+            }
+            Console.WriteLine($"среднее арифметическое столбца {i + 1} = {Math.Round((double)sumByCol / arr.GetLength(0), 2)}");
+            sumByCol = 0;
+            average = 0;
+        }
+    }
+}
---
+++ lessonTasks/Program.cs
@@ -0,0 +1,61 @@
+﻿using static Common.Helper;
+
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Console.WriteLine("Введите кол-во строк");
+// bool isRowsCountInt = int.TryParse(Console.ReadLine(), out int rowsCount);
+// Console.WriteLine("Введите кол-во столбцов");
+// bool isColumnsCountInt = int.TryParse(Console.ReadLine(), out int columnsCount);
+// if (!isColumnsCountInt | !isRowsCountInt)
+// {
+//     Console.WriteLine("некоректное значение");
+// return;
+// }
+// int[,] result2dArr = GetRandom2dArr(rowsCount, columnsCount);
+// PrintArr(result2dArr);
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+// Console.WriteLine("Введите номер строки");
+// bool isRowsCountInt = int.TryParse(Console.ReadLine(), out int rowsNum);
+// Console.WriteLine("Введите номер столбца");
+// bool isColumnsCountInt = int.TryParse(Console.ReadLine(), out int columnsNum);
+// if (!isColumnsCountInt | !isRowsCountInt)
+// {
+//     Console.WriteLine("некоректное значение");
+//     return;
+// }
+// int[,] result2dArr = GetRandom2dArr(GetRandom(2, 10), GetRandom(2, 10));
+// PrintArr(result2dArr);
+// int searchResult = PrintElementBy2dArr(result2dArr, rowsNum, columnsNum);
+
+// if (searchResult == -1)
+// {
+//     Console.WriteLine("такого числа в массиве нет");
+// }
+// else
+// {
+//     Console.WriteLine($"это число - {searchResult}");
+// }
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// int[,] result2dArr = GetRandom2dArr(GetRandom(2, 10), GetRandom(2, 10));
+// PrintArr(result2dArr);
+// PrintAverageBy2dArrCol(result2dArr);
